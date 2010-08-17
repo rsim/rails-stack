e9cfb17f6e8a7f2a5df5cb2b44faa6e58a9feae1
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2010-08-17 19:54:39 +0300

    updated mysql package for redhat/centos

--- lib/stack/mysql.rb
+++ lib/stack/mysql.rb
@@ -1,21 +1,15 @@
 package :mysql, :provides => :database do
   description 'MySQL Database'
-  apt %w( mysql-server mysql-client libmysqlclient-dev )
+
+  case INSTALL_PLATFORM
+  when 'ubuntu'
+    apt %w( mysql-server mysql-client libmysqlclient-dev )
+  when 'redhat', 'centos'
+    yum 'mysql mysql-server mysql-devel'
+  end
 
   verify do
     has_executable 'mysql'
   end
 
-  optional :mysql_driver
-end
-
-package :mysql_driver, :provides => :ruby_database_driver do
-  description 'Ruby MySQL database driver'
-  gem 'mysql'
-
-  verify do
-    has_gem 'mysql'
-  end
-
-  requires :ruby_enterprise
 end
